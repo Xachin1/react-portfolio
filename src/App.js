8c485ed798a34b1199574a179a3cbf9444b82974
Sachin Bhandari <sbhandary123@gmail.com>
2023-06-02 17:26:31 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import { Navigation } from './Components/Navigation';
+import { Hero } from './Components/Hero';
+import { Skills } from './Components/Skills';
+import { Projects } from './Components/Projects';
+import { AboutMe } from './Components/AboutMe';
+import { ContactMe } from './Components/ContactMe';
+import { Footer } from './Components/Footer';
+
+function App() {
+  return (
+    <div>
+        {/* <Navigation/> */}
+        <Hero/>
+        <Skills/>
+        <Projects/>
+        <AboutMe/>
+        <ContactMe/>
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
